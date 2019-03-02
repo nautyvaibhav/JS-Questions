faed6b8b5d8af46283a18ccf26fca8007eec42d2
nautyvaibhav <38046732+nautyvaibhav@users.noreply.github.com>
2019-03-02 14:24:53 +0530

    Update q3.js

--- q3.js
+++ q3.js
@@ -13,24 +13,34 @@ Think about a spinwheel. A spinwheel has different valued items (10Rs, 5 Rs, 2 R
 */
 function minimizeGift() {
 
-    let items = [{
-            1: 0.1
-        },
-        {
-            2: 0.3
-        },
-        {
-            5: 0.2
-        },
-        {
-            10: 0.4
-        },
+    let items = [
+
+        [1, 0.1],
+        [2, 0.3],
+        [5, 0.2],
+        [10, 0.4]
     ]
 
     let a = Math.floor(Math.random() * 4);
-    //console.log(a)
-    console.log(Object.entries(items[a]));
+    console.log(a)
+    let userValue = items[a][0];
+
+    let count = 5;  
+    let random =Math.floor(Math.random()*2);
+    let prev=0;
+    while(count){
+        if(random==1 && prev!==1){
+            prev=a;
+            count--;
+            //give the precious  gift 
+            return userValue;
+        }
+        else{
+// give the normal gifts
+        }
+    }   
+ }
 
 
-}
+
 minimizeGift()
